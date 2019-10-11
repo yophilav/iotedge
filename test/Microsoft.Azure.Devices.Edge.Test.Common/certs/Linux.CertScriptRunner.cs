c769f02fa3d9f21be14d6196ec2bb3ff9749a76a
Bear <yophilav@microsoft.com>
2019-10-11 16:08:10 -0700

    Factoring out cert generation off of IOsPlatform

--- test/Microsoft.Azure.Devices.Edge.Test.Common/certs/Linux.CertScriptRunner.cs
+++ test/Microsoft.Azure.Devices.Edge.Test.Common/certs/Linux.CertScriptRunner.cs
@@ -5,6 +5,7 @@ namespace Microsoft.Azure.Devices.Edge.Test.Common.Certs.Linux
     using System.Threading;
     using System.Threading.Tasks;
     using Microsoft.Azure.Devices.Edge.Test.Common;
+    using Microsoft.Azure.Devices.Edge.Test.Common.Certs;
 
     public class CertScriptRunner : ICertScriptRunner
     {
@@ -15,8 +16,41 @@ namespace Microsoft.Azure.Devices.Edge.Test.Common.Certs.Linux
         {
             var command = BuildCertCommand($"create_device_certificate '{deviceId}'", scriptPath);
             await OsPlatform.RunScriptAsync(("bash", command), token);
+
+            // Verify the cert and key were generated
+            string path = FixedPaths.DeviceIdentityCert.Cert(deviceId);
+            if(!File.Exists(path))
+            {
+                throw new System.ArgumentException($"Fail to generate: {path}");
+            }
+
+            path = FixedPaths.DeviceIdentityCert.Key(deviceId);
+            if(!File.Exists(path))
+            {
+                throw new System.ArgumentException($"Fail to generate: {path}");
+            }
         }
 
+        public async Task GenerateDeviceCaCertAsync(string deviceId, string scriptPath, CancellationToken token)
+        {
+            var command = BuildCertCommand($"create_edge_device_certificate '{deviceId}'", scriptPath);
+            await OsPlatform.RunScriptAsync(("bash", command), token);
 
+            string path = FixedPaths.DeviceCaCert.Cert(deviceId);
+            if(!File.Exists(path))
+            {
+                throw new System.ArgumentException($"Fail to generate: {path}");
+            }
+            path = FixedPaths.DeviceCaCert.Key(deviceId);
+            if(!File.Exists(path))
+            {
+                throw new System.ArgumentException($"Fail to generate: {path}");
+            }
+            path = FixedPaths.DeviceCaCert.TrustCert(deviceId);
+            if(!File.Exists(path))
+            {
+                throw new System.ArgumentException($"File Not Found: {path}");
+            }
+        }
     }
 }
--- test/Microsoft.Azure.Devices.Edge.Test.Common/certs/Windows.CertScriptRunner.cs
+++ test/Microsoft.Azure.Devices.Edge.Test.Common/certs/Windows.CertScriptRunner.cs
@@ -24,10 +24,47 @@ namespace Microsoft.Azure.Devices.Edge.Test.Common.Certs.Windows
             var command = BuildCertCommand($"New-CACertsDevice '{deviceId}'", scriptPath);
             await OsPlatform.RunScriptAsync(("powershell", command), token);
 
+            // Verify the cert and key were generated
+            string path = FixedPaths.DeviceIdentityCert.Cert(deviceId);
+            if(!File.Exists(path))
+            {
+                throw new System.ArgumentException($"Fail to generate: {path}");
+            }
+
+            path = FixedPaths.DeviceIdentityCert.Key(deviceId);
+            if(!File.Exists(path))
+            {
+                throw new System.ArgumentException($"Fail to generate: {path}");
+            }
+
             // BEARWASHERE -- Migrate this to 'Install once it's done
             // Windows requires all the certificates from root up to leaf to be installed.
             await OsPlatform.RunScriptAsync(("powershell", $"Import-Certificate -CertStoreLocation 'cert:\\LocalMachine\\Root' -FilePath " + Path.Combine(scriptPath, "certs", "azure-iot-test-only.root.ca.cert.pem") + " | Out-Host"), token);
             await OsPlatform.RunScriptAsync(("powershell", $"Import-Certificate -CertStoreLocation 'cert:\\LocalMachine\\Root' -FilePath " + Path.Combine(scriptPath, "certs", "azure-iot-test-only.intermediate.cert.pem") + " | Out-Host"), token);
+        }
+
+        public async Task GenerateDeviceCaCertAsync(string deviceId, string scriptPath, CancellationToken token)
+        {
+            string command = BuildCertCommand(
+                $"New-CACertsEdgeDevice '{deviceId}'",
+                scriptPath);
+            await OsPlatform.RunScriptAsync(("powershell", command), token);
+
+            string path = FixedPaths.DeviceCaCert.Cert(deviceId);
+            if(!File.Exists(path))
+            {
+                throw new System.ArgumentException($"Fail to generate: {path}");
+            }
+            path = FixedPaths.DeviceCaCert.Key(deviceId);
+            if(!File.Exists(path))
+            {
+                throw new System.ArgumentException($"Fail to generate: {path}");
+            }
+            path = FixedPaths.DeviceCaCert.TrustCert(deviceId);
+            if(!File.Exists(path))
+            {
+                throw new System.ArgumentException($"File Not Found: {path}");
+            }
         }
     }
 }
