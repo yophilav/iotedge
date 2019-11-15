3720bf9dd9508295be129954d48c7a207ac9480a
Bear <yophilav@microsoft.com>
2019-11-14 16:32:24 -0800

    Successful query

--- tools/IoTEdgeDevOps/DevOpsLib/AzureLogAnalytics.cs
+++ tools/IoTEdgeDevOps/DevOpsLib/AzureLogAnalytics.cs
@@ -15,7 +15,14 @@ namespace DevOpsLib
         // static readonly ILogger Log = Logger.Factory.CreateLogger<AzureLogAnalytics>();
         static readonly AzureLogAnalytics instance = new AzureLogAnalytics();
         static readonly string apiVersion = "v1";
+
+        static bool isAzureActiveDirInited = false;
+        static string azureActiveDirTenant = null;
+        static string azureActiveDirClientId = null;
+        static string azureActiveDirClientSecret = null;
+        static string azureResource = null;
         static string accessToken = null;
+        static DateTime accessTokenExpiration = new DateTime(1970, 01, 01, 0, 0, 0, 0, DateTimeKind.Utc);
 
         // Explicit static constructor to tell C# compiler
         // not to mark type as beforefieldinit
@@ -45,32 +52,85 @@ namespace DevOpsLib
         {
             try
             {
-                string requestUri = $"https://login.microsoftonline.com/{azureActiveDirTenant}/oauth2/token";
-                const string grantType = "client_credentials";
+                if ((DateTime.Compare(DateTime.UtcNow, AzureLogAnalytics.accessTokenExpiration) >= 0) )
+                {
+                    string requestUri = $"https://login.microsoftonline.com/{azureActiveDirTenant}/oauth2/token";
+                    const string grantType = "client_credentials";
 
-                var client = new HttpClient();
-                client.BaseAddress = new Uri(requestUri);
-                //client.DefaultRequestHeaders.Add("Content-Type", "application/x-www-form-urlencoded");
+                    var client = new HttpClient();
+                    client.BaseAddress = new Uri(requestUri);
+                    //client.DefaultRequestHeaders.Add("Content-Type", "application/x-www-form-urlencoded");
 
-                var requestBody = new List<KeyValuePair<string, string>>();
-                requestBody.Add(new KeyValuePair<string, string>("client_id", azureActiveDirClientId));
-                requestBody.Add(new KeyValuePair<string, string>("client_secret", azureActiveDirClientSecret));
-                requestBody.Add(new KeyValuePair<string, string>("grant_type", grantType));
-                requestBody.Add(new KeyValuePair<string, string>("resource", azureResource));
-                
-                HttpRequestMessage request = new HttpRequestMessage(HttpMethod.Post, "");
-                // By default, if FormUrlEncodedContent() is used, the "Content-Type" is set to "application/x-www-form-urlencoded"
-                // request.Content.Headers = new MediaTypeHeaderValue("application/x-www-form-urlencoded");
-                request.Content = new FormUrlEncodedContent(requestBody);
+                    var requestBody = new List<KeyValuePair<string, string>>();
+                    requestBody.Add(new KeyValuePair<string, string>("client_id", azureActiveDirClientId));
+                    requestBody.Add(new KeyValuePair<string, string>("client_secret", azureActiveDirClientSecret));
+                    requestBody.Add(new KeyValuePair<string, string>("grant_type", grantType));
+                    requestBody.Add(new KeyValuePair<string, string>("resource", azureResource));
 
-                var response = await client.SendAsync(request).ConfigureAwait(false);
-                var responseMsg = await response.Content.ReadAsStringAsync().ConfigureAwait(false);
+                    HttpRequestMessage request = new HttpRequestMessage(HttpMethod.Post, "");
+                    // By default, if FormUrlEncodedContent() is used, the "Content-Type" is set to "application/x-www-form-urlencoded"
+                    // request.Content.Headers = new MediaTypeHeaderValue("application/x-www-form-urlencoded");
+                    request.Content = new FormUrlEncodedContent(requestBody);
 
-                return (string) JObject.Parse(responseMsg)["access_token"];
+                    var response = await client.SendAsync(request).ConfigureAwait(false);
+                    var responseMsg = await response.Content.ReadAsStringAsync().ConfigureAwait(false);
+
+                    AzureLogAnalytics.azureActiveDirTenant = azureActiveDirTenant;
+                    AzureLogAnalytics.azureActiveDirClientId = azureActiveDirClientId;
+                    AzureLogAnalytics.azureActiveDirClientSecret = azureActiveDirClientSecret;
+                    AzureLogAnalytics.azureResource = azureResource;
+                    AzureLogAnalytics.isAzureActiveDirInited = true;
+
+                    var responseJson = JObject.Parse(responseMsg);
+                    AzureLogAnalytics.accessTokenExpiration = DateTime.UtcNow.AddSeconds((double)responseJson["expires_on"]);
+                    AzureLogAnalytics.accessToken = (string)responseJson["access_token"];
+                }
+
+                return AzureLogAnalytics.accessToken;
             }
             catch (Exception e)
             {
-                // Log.LogError(e.Message);
+                throw e;
+            }
+        }
+
+        // Use get-request to do a Kusto query
+        // API reference: https://dev.loganalytics.io/documentation/Using-the-API/RequestFormat
+        public async Task<string> GetKqlQuery(
+            string logAnalyticsWorkspaceId,
+            string kqlQuery)
+        {
+            try
+            {
+                if ((DateTime.Compare(DateTime.UtcNow, AzureLogAnalytics.accessTokenExpiration) >= 0) ||
+                   (AzureLogAnalytics.accessToken == null))
+                {
+                    if (AzureLogAnalytics.isAzureActiveDirInited)
+                    {
+                        await AzureLogAnalytics.Instance.GetAccessToken(
+                            AzureLogAnalytics.azureActiveDirTenant,
+                            AzureLogAnalytics.azureActiveDirClientId,
+                            AzureLogAnalytics.azureActiveDirClientSecret,
+                            AzureLogAnalytics.azureResource);
+                    }
+                    else
+                    {
+                        throw new ArgumentException("The access token is expired without Azure Active Directory credentials to renew. Please invoke GetAccessToken(4)", AzureLogAnalytics.accessToken);
+                    }
+                }
+
+                string requestUri = $"https://api.loganalytics.io/{AzureLogAnalytics.apiVersion}/workspaces/{logAnalyticsWorkspaceId}/query?query=";
+
+                var client = new HttpClient();
+                client.DefaultRequestHeaders.Add("Authorization", $"Bearer {AzureLogAnalytics.accessToken}");
+
+                var response = await client.GetAsync(requestUri + kqlQuery).ConfigureAwait(false);
+                var responseMsg = await response.Content.ReadAsStringAsync().ConfigureAwait(false);
+
+                return responseMsg;
+            }
+            catch (Exception e)
+            {
                 throw e;
             }
         }
